7c0b834c02d48664790cc62f814e382ca6e2ad26
maximilianotulian <maxtulian93@hotmail.com>
2015-09-11 18:04:51 -0300

    [Maximiliano Tulian] - 1_5,7 - apply code review feedback

--- 01-js-intro-and-jquery/keypoints/1_5/resources/script/script.js
+++ 01-js-intro-and-jquery/keypoints/1_5/resources/script/script.js
@@ -2,7 +2,7 @@
  * Created by MaximilianoDaniel on 10/09/2015.
  */
 
-$( document ).ready(function() {
+$(document).ready(function () {
 
     $('#section-hide').fadeIn('normal');
 
--- 01-js-intro-and-jquery/keypoints/1_7,6,8,9/resources/script/script.js
+++ 01-js-intro-and-jquery/keypoints/1_7,6,8,9/resources/script/script.js
@@ -2,28 +2,32 @@
  * Created by MaximilianoDaniel on 10/09/2015.
  */
 
-
-
 //Exercise 1_6 begin
-$('section:hidden').fadeIn('normal');
-var inputFocus = $('input').first();
-inputFocus.focus();
+
+var pageReady = function() {
+    var inputFocus = $('input').first();
+    $('section:hidden').fadeIn('normal',function () {
+        inputFocus.focus();
+    });
 
 //Exercise 1_6 end
 
 //Exercise 1_7,8,9 begin
-$('input[type="button"]').click(function () {
-    $.ajax({
-        url: 'http://bootcamp.aws.af.cm/welcome/yourname',
-        success: function (result) {
-            console.log(result.response);
-            inputFocus.val(result.response);
-        },
-        error: function (a, b, c) {
-            inputFocus.parent().addClass('has-error');
-            inputFocus.val('Not found');
-        }
+    $('input[type="button"]').click(function () {
+        $.ajax({
+            url: 'http://bootcamp.aws.af.cm/welcome/yourname',
+            success: function (result) {
+                console.log(result.response);
+                inputFocus.val(result.response);
+            },
+            error: function (a, b, c) {
+                inputFocus.parent().addClass('has-error');
+                inputFocus.val('Not found');
+            }
+        });
     });
-});
+};
+
+$.ready(pageReady());
 //Exercise 1_7,8,9 end
 
