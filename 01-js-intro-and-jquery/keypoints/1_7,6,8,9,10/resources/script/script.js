8816d713a022882fa0c239be8d61a8a91cafe710
maximilianotulian <maxtulian93@hotmail.com>
2015-09-11 22:44:24 -0300

    [Maximiliano Tulian] - 1_6-10  work done

---
+++ 01-js-intro-and-jquery/keypoints/1_7,6,8,9,10/resources/script/script.js
@@ -0,0 +1,37 @@
+/**
+ * Created by MaximilianoDaniel on 10/09/2015.
+ */
+
+
+
+var onPageReady = function () {
+    var input = $('.alias');
+    var button = $('#btn-hit-me');
+    var section = $('section.hidden');
+
+    section.fadeIn('normal', function () {
+        input.focus();
+    });
+
+
+
+    button.click(function () {
+        var inputValue = input.val() || 'guest';
+
+        $.ajax({
+            url: 'http://bootcamp.aws.af.cm/welcome/' + inputValue,
+            success: function (result) {
+                section.text(result.response);
+            },
+            error: function (a, b, c) {
+                section.addClass('has-error');
+                section.text('not found');
+            }
+        });
+    });
+};
+
+$(document).on('ready', onPageReady);
+
+
+
