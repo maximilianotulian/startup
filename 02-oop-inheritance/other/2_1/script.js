6b130d3e675c9a180307cbd82a55d9b359c3482e
maximilianotulian <maxtulian93@hotmail.com>
2015-09-16 17:47:37 -0300

    [Maximiliano Tulian] - 2.1-2  Done

--- 02-oop-inheritance/other/2_1/script.js
+++ 02-oop-inheritance/other/2_1/script.js
@@ -1,8 +1,38 @@
 /**
  * Created by MaximilianoDaniel on 15/09/2015.
  */
-var Movie = function () {
-    this.hashMap = "";
-    console.log('A movie was created');
+//Constructor and prototype pattern
+var Movie = function (title, genre, rating ) {
+
+    this.genre = genre || '' ;
+    this.rating = rating || 0 ;
+    this.title = title || '' ;
+    console.log('A movie called ' + title + ' was created');
 };
 
+Movie.prototype = {
+    constructor: Movie,
+    get: function (key) {
+        return this[key];
+    },
+    play: function () {
+        console.log('Now playing: ' + this.title);
+    },
+    set: function(key, value) {
+        this[key] = value;
+    },
+    stop: function () {
+        console.log('Stopping: ' + this.title);
+    }
+};
+
+//Movies to play in the console
+var superman = new Movie('superman', 'drama', 4);
+var harryPotter = new Movie('harry potter', 'ficcion', 5);
+
+superman.play();
+superman.stop();
+console.log(superman.title + ' ' + superman.genre + ' ' + superman.rating);
+console.log(harryPotter);
+superman.set('title', 'iron man');
+console.log(superman.get('title'));
