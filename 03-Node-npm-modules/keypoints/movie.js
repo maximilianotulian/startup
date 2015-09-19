ff15148e9899e9c0c5b7ffe18c97ad1fd83c778a
maximilianotulian <maxtulian93@hotmail.com>
2015-09-18 23:13:23 -0300

    [Maximiliano Tulian] - 3.1-3 Done

---
+++ 03-Node-npm-modules/keypoints/movie.js
@@ -0,0 +1,22 @@
+/**
+ * Created by MaximilianoDaniel on 18/09/2015.
+ */
+
+var Movie = function (title, genre, rating ) {
+
+    this.genre = genre || '' ;
+    this.rating = rating || 0 ;
+    this.title = title || '' ;
+
+    console.log('A movie called ' + title + ' was created');
+};
+
+Movie.prototype.get = function (key) {
+    return this[key];
+};
+
+Movie.prototype.set = function(key, value) {
+    this[key] = value;
+};
+
+exports.movie = Movie;
