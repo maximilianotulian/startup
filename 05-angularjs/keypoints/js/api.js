c557aa54f12afdb6d5fcc14409a1e649e407ffe1
maximilianotulian <maxtulian93@hotmail.com>
2015-09-23 16:41:42 -0300

    [Maximiliano Tulian] - 5_3 Done

--- 05-angularjs/keypoints/js/api.js
+++ 05-angularjs/keypoints/js/api.js
@@ -1,40 +1,84 @@
 /**
  * Created by MaximilianoDaniel on 21/09/2015.
  */
-var MovieApp = angular.module('MovieApp', []);
+var MovieApp = angular.module('MovieApp', ['ngRoute']);
+
+MovieApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/index/:movieIndex/action/:action', {
+        templateUrl: 'template/abmMovie.html',
+        controller: 'MovieAbmCtrl'
+        });
+}]);
 
 MovieApp.controller('MovieListCtrl', ['$scope', 'Movies', function ($scope, Movies) {
+    Movies.clear();
     $scope.movies = Movies.get();
 }]);
 
-MovieApp.service('Movies', function () {
+MovieApp.service('Movies', function ($localStorage) {
+    this.movies = [];
+
+    this.init = function () {
+        this.movies = [{
+            "title": "Harry Potter and the philosopher's stone",
+            "genre": "ficcion",
+            "rating": 5
+        }, {
+            "title": "Iron Man",
+            "genre": "arte",
+            "rating": 4
+        }, {
+            "title": "Mary Poppins",
+            "genre": "ciencia",
+            "rating": 3
+        }, {
+            "title": "Harry Potter and the chamber of secrets",
+            "genre": "drama",
+            "rating": 2
+        }];
+        $localStorage.setObject('movies', this.movies);
+    };
     this.get = function () {
-        var movies = [];
-        if (window.localStorage['movies']) {
-            movies = JSON.parse(window.localStorage['movies']);
+        if ($localStorage.getObject('movies') !== null) {
+            this.movies = $localStorage.getObject('movies');
         } else {
-            movies = [{
-                "title": "Harry Potter and the philosopher's stone",
-                "genre": "ficcion",
-                "rating": 5
-            }, {
-                "title": "Iron Man",
-                "genre": "ficcion",
-                "rating": 5
-            }, {
-                "title": "Mary Poppins",
-                "genre": "comedia",
-                "rating": 5
-            }, {
-                "title": "Harry Potter and the chamber of secrets",
-                "genre": "drama",
-                "rating": 5
-            }];
-            window.localStorage['movies'] = JSON.stringify(movies);
+            this.init();
         }
-        return movies;
+        return this.movies;
+    };
+    this.push = function (movie) {
+        this.movies.push(movie);
+        $localStorage.setObject('movies', this.movies);
+    };
+    this.delete = function (index) {
+        this.movies.splice(index, 1);
+        $localStorage.setObject('movies', this.movies);
+    };
+    this.edit = function (index, movie) {
+        this.movies[index] = movie;
+        $localStorage.setObject('movies', this.movies);
+    };
+    this.getAtIndex = function (index) {
+        return this.movies[index];
+    };
+    this.clear = function () {
+      $localStorage.clear();
     };
 });
+
+MovieApp.factory('$localStorage', ['$window', function($window) {
+    return {
+        setObject: function(key, value) {
+            $window.localStorage[key] = JSON.stringify(value);
+        },
+        getObject: function(key) {
+            return JSON.parse($window.localStorage[key] || null );
+        },
+        clear: function() {
+            $window.localStorage.clear();
+        }
+    }
+}]);
 
 MovieApp.directive('movieDetails', function () {
     return {
@@ -42,6 +86,42 @@ MovieApp.directive('movieDetails', function () {
     };
 });
 
-MovieApp.controller('MovieAbmCtrl', ['$scope', function ($scope) {
+MovieApp.controller('MovieAbmCtrl', ['$scope', '$routeParams', 'Movies', function ($scope, $routeParams, Movies) {
 
+    this.setMovieScope = function (movie) {
+        $scope.title = movie.title;
+        $scope.genre = movie.genre;
+        $scope.rating = movie.rating;
+    };
+
+    $scope.movieIndex = $routeParams.movieIndex;
+    $scope.action = $routeParams.action;
+    $scope.saveChanges = function () {
+        if ($scope.action == 'add') {
+            var movie = {
+
+                title: $scope.title,
+                genre: $scope.genre,
+                rating: $scope.rating
+            };
+            Movies.push(movie);
+        } else if ($scope.action == 'delete') {
+            Movies.delete($scope.movieIndex);
+        } else if ($scope.action == 'edit') {
+            var movie = {
+
+                title: $scope.title,
+                genre: $scope.genre,
+                rating: $scope.rating
+            };
+            Movies.edit($scope.movieIndex, movie);
+        };
+        $scope.title = '';
+        $scope.genre = '';
+        $scope.rating = '';
+    };
+
+    if ($scope.action != 'add') {
+        this.setMovieScope(Movies.getAtIndex($scope.movieIndex));
+    };
 }]);
